b2193a6abc0f09d9d08e06d9ff3e4097c95842bb
mojo_jojo <mojo_jojo>
2004-08-17 22:25:15 +0000

    Added comments.

--- org.va-labs.vae/src/org/va_labs/vae/Messages.java
+++ org.va-labs.vae/src/org/va_labs/vae/Messages.java
@@ -1,7 +1,7 @@
 /*
  * Created on Jul 25, 2004
  *
- * $Id: Messages.java,v 1.1 2004/08/07 10:15:17 mojo_jojo Exp $
+ * $Id: Messages.java,v 1.2 2004/08/17 22:25:15 mojo_jojo Exp $
  */
 package org.va_labs.vae;
 
@@ -10,7 +10,7 @@ import java.util.ResourceBundle;
 
 /**
  * @author mojo_jojo
- * TODO Comment the Messages class.
+ * Messages class handling vae strings for i18n..
  */
 public class Messages {
     private static final String BUNDLE_NAME = "org.va_labs.vae.messages";//$NON-NLS-1$
