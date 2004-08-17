e3257b2c07672be3fb23e0e20730aaeb60dcc2da
mojo_jojo <mojo_jojo>
2004-08-17 22:36:10 +0000

    Initial commit with just what is needed to get started.

---
+++ org.va-labs.vae/src/org/va_labs/vae/core/Vae.java
@@ -0,0 +1,15 @@
+/*
+ * Created on Aug 15, 2004
+ *
+ * $Id: Vae.java,v 1.1 2004/08/17 22:36:10 mojo_jojo Exp $
+ */
+package org.va_labs.vae.core;
+
+/**
+ * @author mojo_jojo
+ * The Visual Ant Editor core functionality is here.
+ */
+public class Vae {
+
+    public static final int VAE__OK = 0;
+}
