b2a9e6f0bfe883b7631c440b5559cb7c6b1e901a
mojo_jojo <mojo_jojo>
2004-09-05 20:36:31 +0000

    (createPartControl): Took out the creation of a vae instance as it is done when the WorkbenchAdvisor initializes vae. (DataTreeView): Registers the view in the Vui so it can set the input of the tree when needed. (setTreeInput): This new method enables to define the input for the project tree of the DataTreeView.

--- org.va-labs.vae/src/org/va_labs/vae/gui/view/datatree/DataTreeView.java
+++ org.va-labs.vae/src/org/va_labs/vae/gui/view/datatree/DataTreeView.java
@@ -1,7 +1,7 @@
 /*
  * Created on Aug 4, 2004
  *
- * $Id: DataTreeView.java,v 1.1 2004/09/05 00:23:22 mojo_jojo Exp $
+ * $Id: DataTreeView.java,v 1.2 2004/09/05 20:36:31 mojo_jojo Exp $
  */
 package org.va_labs.vae.gui.view.datatree;
 
@@ -11,49 +11,67 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 import org.va_labs.vae.Messages;
-import org.va_labs.vae.core.Vae;
-import org.va_labs.vae.gui.SwtVae;
+import org.va_labs.vae.gui.Vui;
 
 /**
  * @author mojo_jojo
  * 
  * This view shows a tree of the differents tags of the ant project file.
  */
-public class DataTreeView extends ViewPart 
-{
-    public final static String ID = 
-        "org.va_labs.vae.gui.view.datatree.DataTreeView";
-    
+public class DataTreeView extends ViewPart {
+    public final static String ID = "org.va_labs.vae.gui.view.datatree.DataTreeView";
+
+    /**
+     * Reference to the treeViewer displaying the project Tree.
+     */
+    private TreeViewer projectTree;
+
+    /**
+     * Register the view to the vui instance.
+     */
+    public DataTreeView() {
+        Vui vui = Vui.getVui();
+        vui.registerTreeView(this);
+    }
+
     /**
      * Creates the view.
-     * @param parent parent Composite of the view to be created.
+     * 
+     * @param parent
+     *            parent Composite of the view to be created.
      * @see org.eclipse.ui.IWorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
      */
-    public void createPartControl(Composite parent) 
-    {
+    public void createPartControl(Composite parent) {
         // Indicates the title of the view.
         setPartName(Messages.getString("Data_Tree"));
-        
+
         parent.setLayout(new FillLayout());
-        
+
         // Setting up the TreeViewer.
-		TreeViewer treeViewer = new TreeViewer(parent, 
-			SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
-		
-		// Initialise the main component and it's Swt extention.
-		SwtVae swtVae = new SwtVae(new Vae());
-		
-		treeViewer.setContentProvider(new ProjectTreeContentProvider());
-		treeViewer.setLabelProvider(new ProjectTreeLabelProvider());
-		treeViewer.setInput(swtVae);
+        projectTree = new TreeViewer(parent, SWT.BORDER | SWT.MULTI
+                | SWT.V_SCROLL | SWT.H_SCROLL);
+
+        projectTree.setContentProvider(new ProjectTreeContentProvider());
+        projectTree.setLabelProvider(new ProjectTreeLabelProvider());
     }
 
     /**
      * This method is called upon the view being rendered within the workbench.
+     * 
      * @see org.eclipse.ui.IWorkbenchPart#setFocus()
      */
-    public void setFocus() 
-    {
-        
+    public void setFocus() {
+
+    }
+
+    /**
+     * Sets a new input for the project tree.
+     * 
+     * @param input
+     *            new input to be used by the content provider for the project
+     *            tree.
+     */
+    public void setTreeInput(Object input) {
+        projectTree.setInput(input);
     }
 }
