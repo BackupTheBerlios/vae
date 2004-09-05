bd72b70db34424422971728120b724521a62a57a
mojo_jojo <mojo_jojo>
2004-09-05 20:34:53 +0000

    (Vui): The constructor is now private. It registers the vae core instance, and register itself to it. (getVui): Added this method that returns the core vui intance. The instance is created if needed. (registerConfigurer): The IWorkbenchWindowConfigurer of the gui can be registered through this new method. (registerTreeView): This new methods registers the DataTreeView for the vui. (displayProject): The method updates the input of the project tree.

--- org.va-labs.vae/src/org/va_labs/vae/gui/Vui.java
+++ org.va-labs.vae/src/org/va_labs/vae/gui/Vui.java
@@ -1,7 +1,7 @@
 /*
  * Created on Aug 16, 2004
  *
- * $Id: Vui.java,v 1.2 2004/09/05 00:15:52 mojo_jojo Exp $
+ * $Id: Vui.java,v 1.3 2004/09/05 20:34:53 mojo_jojo Exp $
  */
 package org.va_labs.vae.gui;
 
@@ -16,177 +16,252 @@ import org.eclipse.ui.dialogs.ListSelectionDialog;
 import org.va_labs.vae.core.Vae;
 import org.va_labs.vae.gui.dialog.SaveFilesContentProvider;
 import org.va_labs.vae.gui.dialog.SaveFilesLabelProvider;
+import org.va_labs.vae.gui.tag.project.SwtProject;
+import org.va_labs.vae.gui.view.datatree.DataTreeView;
 import org.va_labs.vae.tag.project.Project;
 
 /**
  * @author mojo_jojo
- * Vae User Interface level methods.
+ * 
+ * Vae User Interface class. Core user interface methods.
  */
 public class Vui {
-    
+
+    /**
+     * Reference to the unique vui instance that pilotes the user interface.
+     */
+    private static Vui vui = null;
+
+    /**
+     * Enables to get the vui instance driving the user interface, The instance
+     * is created if needed.
+     * 
+     * @return the vui instance piloting the user interface.
+     */
+    public static Vui getVui() {
+        if (vui == null) {
+            vui = new Vui();
+        }
+        return vui;
+    }
+
+    /**
+     * Reference to the dataTreeView.
+     */
+    private DataTreeView dataTreeView;
+
     /**
      * Reference to the core vae object.
      */
     private Vae vae;
-    
+
     /**
      * Workbench Configurer used to get access to gui subcomponents.
      */
     private IWorkbenchWindowConfigurer windowConfigurer;
-    
+
     /**
-     * Saves a reference to the core vae component, and the main window
-     * of the user interface for further shell access.
-     * @param coreVae core vae component related to this gui.
-     * @param configurer IWorkbenchWindowConfigurer used to get access to gui
-     * subcomponents.
+     * Initialization of the Vae User Interface. Registers the vae core, and
+     * registers this instance to the vae core.
+     *  
      */
-    public Vui(Vae coreVae, IWorkbenchWindowConfigurer configurer) {
-        vae = coreVae;
-        windowConfigurer = configurer;
+    private Vui() {
+        vae = Vae.getVae();
+        vae.registerVui(this);
     }
-    
+
     /**
-	 * Prompts an error to the user.
-	 * @param vaeModule The module that issued this error.
-	 * @param errorMessage The error message.
-	 * @param reasonMessage The reason for this error message.
-	 * @param moduleStatus The status of the issuing module after the error.
-	 * @param e The exception that was raised because of the error (just send null
-	 * if no exception was the source of it).
-	 * @see org.eclipse.core.runtime.IStatus
-	 */
-	public void acknowledgeError(String vaeModule, String errorMessage, 
-			String reasonMessage, int moduleStatus, Exception e) {
-		if (reasonMessage == null) {
-			reasonMessage = e.getCause().toString();
-		}
-		
-		Status status = new Status(IStatus.ERROR, vaeModule, 
-				moduleStatus, reasonMessage, e);
-		
-		ErrorDialog.openError(windowConfigurer.getWindow().getShell(), 
-		        "Visual Ant Editor -- " + vaeModule + " error", errorMessage,
-		        status, IStatus.ERROR);
-	}
-    
-	/**
-	 * Prompts a warning to the user.
-	 * @param vaeModule The module that issued this warning.
-	 * @param warningMessage The warning message.
-	 * @param reasonMessage The reason for this warning message.
-	 * @param moduleStatus The status of the issuing module after the warning.
-	 * @param e The exception that was raised to issue this warning (just send null if 
-	 * no exception was the source of it).
-	 * @see org.eclipse.core.runtime.IStatus
-	 */
-	public void acknowledgeWarning(String vaeModule, String warningMessage, 
-	        String reasonMessage, int moduleStatus, Exception e) {
-	    // Creates a swt status for the swt warning dialog
-		Status status = new Status(IStatus.WARNING, vaeModule, 
-				moduleStatus, reasonMessage, e);
-		
-		ErrorDialog.openError(windowConfigurer.getWindow().getShell(), 
-		        "Visual Ant Editor -- "  +vaeModule+ "  warning", 
-		        warningMessage, status, IStatus.WARNING);
-	}
-	
-	/**
-	 * Displays a given project in the gui,
-	 * @param project project to be displayed.
-	 */
-	public void displayProject(Project project) {
-	    // TODO : Implement displayProject.
-	}
-	
+     * Prompts an error to the user.
+     * 
+     * @param vaeModule
+     *            The module that issued this error.
+     * @param errorMessage
+     *            The error message.
+     * @param reasonMessage
+     *            The reason for this error message.
+     * @param moduleStatus
+     *            The status of the issuing module after the error.
+     * @param e
+     *            The exception that was raised because of the error (just send
+     *            null if no exception was the source of it).
+     * @see org.eclipse.core.runtime.IStatus
+     */
+    public void acknowledgeError(String vaeModule, String errorMessage,
+            String reasonMessage, int moduleStatus, Exception e) {
+        if (reasonMessage == null) {
+            reasonMessage = e.getCause().toString();
+        }
+
+        Status status = new Status(IStatus.ERROR, vaeModule, moduleStatus,
+                reasonMessage, e);
+
+        ErrorDialog.openError(windowConfigurer.getWindow().getShell(),
+                "Visual Ant Editor -- " + vaeModule + " error", errorMessage,
+                status, IStatus.ERROR);
+    }
+
+    /**
+     * Prompts a warning to the user.
+     * 
+     * @param vaeModule
+     *            The module that issued this warning.
+     * @param warningMessage
+     *            The warning message.
+     * @param reasonMessage
+     *            The reason for this warning message.
+     * @param moduleStatus
+     *            The status of the issuing module after the warning.
+     * @param e
+     *            The exception that was raised to issue this warning (just send
+     *            null if no exception was the source of it).
+     * @see org.eclipse.core.runtime.IStatus
+     */
+    public void acknowledgeWarning(String vaeModule, String warningMessage,
+            String reasonMessage, int moduleStatus, Exception e) {
+        // Creates a swt status for the swt warning dialog
+        Status status = new Status(IStatus.WARNING, vaeModule, moduleStatus,
+                reasonMessage, e);
+
+        ErrorDialog.openError(windowConfigurer.getWindow().getShell(),
+                "Visual Ant Editor -- " + vaeModule + "  warning",
+                warningMessage, status, IStatus.WARNING);
+    }
+
+    /**
+     * Displays a given project in the gui,
+     * 
+     * @param project
+     *            project to be displayed.
+     */
+    public void displayProject(Project project) {
+        dataTreeView.setTreeInput(new SwtProject(project));
+    }
+
     /**
      * Gets the currently active project.
+     * 
      * @return the Project object referencing the active project.
      */
     public Project getActiveProject() {
-        System.out.println(
-                "Gui has been requested the name of the active project");
+        System.out
+                .println("Gui has been requested the name of the active project");
         return null;
     }
-    
+
     /**
      * Enables access to the shell of this gui.
+     * 
      * @return the shell of this gui.
      */
     public Shell getShell() {
         return windowConfigurer.getWindow().getShell();
     }
-    
+
     /**
-     * Presents a dialog with the resources that needs to be saved as
-	 * a list of checkboxed items. 
-	 * The dialog shows up only if at least one project needs to be saved. 
-	 * @return an array of projects that the user asked to save.
+     * Presents a dialog with the resources that needs to be saved as a list of
+     * checkboxed items. The dialog shows up only if at least one project needs
+     * to be saved.
+     * 
+     * @return an array of projects that the user asked to save.
      */
     public Object[] getToSave() {
-        ListSelectionDialog dlg = new ListSelectionDialog(
-                windowConfigurer.getWindow().getShell(),
-                vae, 
-                new SaveFilesContentProvider(), 
-                new SaveFilesLabelProvider(),
-                "Select the resources to save : ");
-		dlg.setTitle("Resources to save.");
-		dlg.setMessage("Select the resources that should be saved : ");
-		dlg.setInitialSelections(vae.getDirtyProjects().toArray());
-		dlg.open();
-		return dlg.getResult();
+        ListSelectionDialog dlg = new ListSelectionDialog(windowConfigurer
+                .getWindow().getShell(), vae, new SaveFilesContentProvider(),
+                new SaveFilesLabelProvider(), "Select the resources to save : ");
+        dlg.setTitle("Resources to save.");
+        dlg.setMessage("Select the resources that should be saved : ");
+        dlg.setInitialSelections(vae.getDirtyProjects().toArray());
+        dlg.open();
+        return dlg.getResult();
     }
-    
+
     /**
      * Opens a build file and displays its content.
-     * @param filename path to the build file to be loaded.
+     * 
+     * @param filename
+     *            path to the build file to be loaded.
      */
     public void openProject(String filename) {
         vae.openProject(filename);
     }
-    
+
     /**
      * Handles a proper shutdown of the Visual Ant Editor.
      */
     public void quit() {
-       System.out.println("Gui has been requested to close Vae"); 
+        System.out.println("Gui has been requested to close Vae");
     }
-    
+
+    /**
+     * Registers the IWorkbenchWindowConfigurer used for the user interface.
+     * It's provided by the eclipse toolkit from the RcpWorkbench.
+     * 
+     * @param configurer
+     *            IworkbenchWindowConfigurer that will configure this interface.
+     */
+    public void registerConfigurer(IWorkbenchWindowConfigurer configurer) {
+        windowConfigurer = configurer;
+    }
+
+    /**
+     * Registers the DataTreeView for this vui.
+     * 
+     * @param treeView
+     *            the DataTreeView in charge of the tree view.
+     */
+    public void registerTreeView(DataTreeView treeView) {
+        dataTreeView = treeView;
+    }
+
+    /**
+     * Register the core vae instance that pilots this user interface.
+     * 
+     * @param coreVae
+     *            vae instance that pilots the vui instance.
+     */
+    public void registerVae(Vae coreVae) {
+        vae = coreVae;
+    }
+
     /**
      * Saves a given project in a specified file.
-     * @param project name of the project to be saved.
-     * @param file file in which the project needs to be saved.
+     * 
+     * @param project
+     *            name of the project to be saved.
+     * @param file
+     *            file in which the project needs to be saved.
      */
     public void saveProject(Project project, String file) {
-        System.out.println("Gui has been requested to save "+project+
-                "in "+file);
+        System.out.println("Gui has been requested to save " + project + "in "
+                + file);
     }
-    
+
     /**
-	 * Signify an error message to the user.
-	 * The message is displayed in the status bar in red.
-	 * @param errorMessage
-	 */
-	public void signifyError(String errorMessage)
-	{
-	    IActionBarConfigurer actionConfigurer = 
-	        windowConfigurer.getActionBarConfigurer();
-	    IStatusLineManager statusManager = 
-	        actionConfigurer.getStatusLineManager();
-		statusManager.setErrorMessage(errorMessage);
-	}
-    
+     * Signify an error message to the user. The message is displayed in the
+     * status bar in red.
+     * 
+     * @param errorMessage
+     */
+    public void signifyError(String errorMessage) {
+        IActionBarConfigurer actionConfigurer = windowConfigurer
+                .getActionBarConfigurer();
+        IStatusLineManager statusManager = actionConfigurer
+                .getStatusLineManager();
+        statusManager.setErrorMessage(errorMessage);
+    }
+
     /**
-	 * Signify a message to the user the message is displayed in the status bar.  
-	 * @param message message to be displayed to the user.
-	 */
-	public void signifyMessage(String message) 
-	{
-	    IActionBarConfigurer actionConfigurer = 
-	        windowConfigurer.getActionBarConfigurer();
-	    IStatusLineManager statusManager = 
-	        actionConfigurer.getStatusLineManager();
-		statusManager.setMessage(message);
-		statusManager.update(true);
-	}
+     * Signify a message to the user the message is displayed in the status bar.
+     * 
+     * @param message
+     *            message to be displayed to the user.
+     */
+    public void signifyMessage(String message) {
+        IActionBarConfigurer actionConfigurer = windowConfigurer
+                .getActionBarConfigurer();
+        IStatusLineManager statusManager = actionConfigurer
+                .getStatusLineManager();
+        statusManager.setMessage(message);
+        statusManager.update(true);
+    }
 }
