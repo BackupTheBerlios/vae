ed6240befd60b10d663fb9915802e293c0ba395f
mojo_jojo <mojo_jojo>
2004-09-05 00:18:45 +0000

    Empty structural commit. Should be filled from standalone version shortly.

---
+++ org.va-labs.vae/src/org/va_labs/vae/gui/processes/VaeExport.java
@@ -0,0 +1,15 @@
+/*
+ * Created on Sep 4, 2004
+ *
+ * $Id: VaeExport.java,v 1.1 2004/09/05 00:18:45 mojo_jojo Exp $
+ */
+package org.va_labs.vae.gui.processes;
+
+/**
+ * @author mojo_jojo
+ *
+ * TODO Comment the  class.
+ */
+public class VaeExport {
+
+}
