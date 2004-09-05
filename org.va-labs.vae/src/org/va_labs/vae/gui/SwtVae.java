c2ff65649723401e5205d9ea05c4c92019757729
mojo_jojo <mojo_jojo>
2004-09-05 20:32:36 +0000

    Formating and put some comments.

--- org.va-labs.vae/src/org/va_labs/vae/gui/SwtVae.java
+++ org.va-labs.vae/src/org/va_labs/vae/gui/SwtVae.java
@@ -1,7 +1,7 @@
 /*
  * Created on Aug 19, 2004
  *
- * $Id: SwtVae.java,v 1.1 2004/09/05 00:15:15 mojo_jojo Exp $
+ * $Id: SwtVae.java,v 1.2 2004/09/05 20:32:36 mojo_jojo Exp $
  */
 package org.va_labs.vae.gui;
 
@@ -11,25 +11,27 @@ import org.va_labs.vae.parser.IllegalAttributeException;
 import org.va_labs.vae.tag.NoSuchAttributeException;
 
 /**
- * @author mojo_jojo
- * Adds Swt methods to the core Vae class.
- * This class will serve as input for the projects tree.
+ * @author mojo_jojo Adds Swt methods to the core Vae class. This class will
+ *         serve as input for the projects tree. TODO : See what to do with the
+ *         SwtVae. Shall it become the tree input provider.
  */
 public class SwtVae implements ISwtElement {
-    
+
     /**
      * Reference to the core instance of Vae that this class extends for swt.
      */
     private Vae vae;
-    
-    public SwtVae( Vae vae) {
+
+    public SwtVae(Vae vae) {
         this.vae = vae;
     }
 
     /**
-     * Vae doesn't have any attributes for now. 
-     * It could be used later as a way to handle the vae preferences.
-     * @see org.va_labs.vae.gui.tag.ISwtElement#addAttribute(java.lang.String, java.lang.String)
+     * Vae doesn't have any attributes for now. It could be used later as a way
+     * to handle the vae preferences.
+     * 
+     * @see org.va_labs.vae.gui.tag.ISwtElement#addAttribute(java.lang.String,
+     *      java.lang.String)
      */
     public void addAttribute(String attributeName, String attributeValue)
             throws IllegalAttributeException {
@@ -37,8 +39,9 @@ public class SwtVae implements ISwtElement {
     }
 
     /**
-     * Vae doesn't have any attributes for now. 
-     * It could be used later as a way to handle the vae preferences.
+     * Vae doesn't have any attributes for now. It could be used later as a way
+     * to handle the vae preferences.
+     * 
      * @see org.va_labs.vae.gui.tag.ISwtElement#getAttributes()
      */
     public Object[] getAttributes() {
@@ -46,17 +49,18 @@ public class SwtVae implements ISwtElement {
     }
 
     /**
-     * Returns the children items (ie projects) that have to be displayed in
-     * the projects tree.
+     * Returns the children items (ie projects) that have to be displayed in the
+     * projects tree.
+     * 
      * @see org.va_labs.vae.gui.tag.ISwtElement#getChildren()
      */
     public Object[] getChildren() {
-        // TODO Implement getChildren
         return new Object[0];
     }
 
-    /** 
+    /**
      * This element is the root of the tree.
+     * 
      * @return this to indicate that we don't have any parent element.
      * @see org.va_labs.vae.gui.tag.ISwtElement#getParent()
      */
@@ -66,35 +70,38 @@ public class SwtVae implements ISwtElement {
 
     /**
      * This one is not used, but we provide a little something.
+     * 
      * @see org.va_labs.vae.gui.tag.ISwtElement#getText()
      */
     public String getText() {
         return "SwtVae";
     }
-    
-    /** 
-     * Indicates wether there are children items (ie projects) to be
-     * displayed in the tree.
+
+    /**
+     * Indicates wether there are children items (ie projects) to be displayed
+     * in the tree.
+     * 
      * @see org.va_labs.vae.gui.tag.ISwtElement#hasChildren()
      */
     public boolean hasChildren() {
-        // TODO Implement hasChildren.
         return false;
     }
 
     /**
-     * Vae doesn't have any attributes for now. 
-     * It could be used later as a way to handle the vae preferences.
+     * Vae doesn't have any attributes for now. It could be used later as a way
+     * to handle the vae preferences.
+     * 
      * @see org.va_labs.vae.gui.tag.ISwtElement#removeAttribute(java.lang.String)
      */
     public void removeAttribute(String attributeName)
             throws NoSuchAttributeException {
-        
+
     }
 
     /**
-     * Vae doesn't have any attributes for now. 
-     * It could be used later as a way to handle the vae preferences.
+     * Vae doesn't have any attributes for now. It could be used later as a way
+     * to handle the vae preferences.
+     * 
      * @see org.va_labs.vae.gui.tag.ISwtElement#setAttributes(java.lang.Object[])
      */
     public void setAttributes(Object[] attributes)
@@ -105,6 +112,7 @@ public class SwtVae implements ISwtElement {
     /**
      * Don't do anythign as the text doesn't get displayed because this is the
      * root attribute.
+     * 
      * @see org.va_labs.vae.gui.tag.ISwtElement#setText(java.lang.String)
      */
     public void setText(String newName) {
@@ -112,9 +120,11 @@ public class SwtVae implements ISwtElement {
     }
 
     /**
-     * Vae doesn't have any attributes for now. 
-     * It could be used later as a way to handle the vae preferences.
-     * @see org.va_labs.vae.gui.tag.ISwtElement#updateAttribute(java.lang.String, java.lang.String, java.lang.String)
+     * Vae doesn't have any attributes for now. It could be used later as a way
+     * to handle the vae preferences.
+     * 
+     * @see org.va_labs.vae.gui.tag.ISwtElement#updateAttribute(java.lang.String,
+     *      java.lang.String, java.lang.String)
      */
     public void updateAttribute(String originalName, String newName,
             String value) throws NoSuchAttributeException,
--- org.va-labs.vae/src/org/va_labs/vae/gui/view/datatree/ProjectTreeContentProvider.java
+++ org.va-labs.vae/src/org/va_labs/vae/gui/view/datatree/ProjectTreeContentProvider.java
@@ -1,7 +1,7 @@
 /*
  * Created on Aug 13, 2004
  *
- * $Id: ProjectTreeContentProvider.java,v 1.1 2004/09/05 00:23:22 mojo_jojo Exp $
+ * $Id: ProjectTreeContentProvider.java,v 1.2 2004/09/05 20:37:28 mojo_jojo Exp $
  */
 package org.va_labs.vae.gui.view.datatree;
 
@@ -10,90 +10,91 @@ import org.eclipse.jface.viewers.Viewer;
 import org.va_labs.vae.gui.tag.ISwtElement;
 
 /**
- * @author mojo_jojo
- * This class handles the content of the Project Tree.
+ * @author mojo_jojo This class handles the content of the Project Tree.
  */
 public class ProjectTreeContentProvider implements ITreeContentProvider {
-    
+
     /**
-     * Disposes of this ContentProvider.
-     * For now nothing to be disposed.
+     * Disposes of this ContentProvider. For now nothing to be disposed.
+     * 
      * @see org.eclipse.jface.viewers.IContentProvider#dispose()
      */
     public void dispose() {
 
     }
 
-    /** 
-	 * Returns children (if any) of the object given as argument.
-	 * @param arg0 the TreeItem whom we want the children.
-	 * @return The children in a Object array.
-	 */
-	public Object[] getChildren(Object object) {
-	    System.out.println("Call to getChildren captured.");
-		if(object instanceof ISwtElement) {
-			return ((ISwtElement) object).getChildren();
-		} else {
-			// TODO Implement the exception handler and use it here.
-		    return null;
-		}
-	}
-
-	/**
-	 * Returns the parent of the object given as argument.
-	 * @param arg0 the TreeItem whom we want the parent.
-	 * @return the parent as an Object.
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
-	 */
-	public Object getParent(Object arg0) 
-	{
-	    System.out.println("Call to getParent captured.");
-		if(arg0 instanceof ISwtElement) {
-			return ((ISwtElement) arg0).getParent();
-		} else {
-		    	// TODO Implement the exception handler and use it here.
-		    return null;
-		}
-	}
-
-	/**
-	 * Indicates wether or not an element has children in the tree.
-	 * @param object the element we wish to have an answer on.
-	 * @return true if the element has children.
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
-	 */
-	public boolean hasChildren(Object object) {
-	    System.out.println("Call to hasChildren captured");
-		if(object instanceof ISwtElement) {
-			return ((ISwtElement) object).hasChildren();
-		} else {
-		    // TODO Implement the exception handler and use it here.
-			return false;
-		}
-	}
-
-	/**
-	 * Called whenever a TreeViewer.setinput() call is made.
-	 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
-	 */
-	public Object[] getElements(Object object) 
-	{
-	    System.out.println("Call to getElements captured "+object);
-		if (object instanceof ISwtElement) {
-			return ((ISwtElement) object).getChildren();
-		} 
-		// TODO Implement the exception handler and use it here.
-	    return null;
-	}
-	
     /**
-     * Notifies this content provider that the given viewer's input  has been 
-     * switched to a different element.
-     * For now this is not used.
-     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+     * Returns children (if any) of the object given as argument.
+     * 
+     * @param arg0
+     *            the TreeItem whom we want the children.
+     * @return The children in a Object array.
+     */
+    public Object[] getChildren(Object object) {
+        if (object instanceof ISwtElement) {
+            return ((ISwtElement) object).getChildren();
+        } else {
+            // TODO Implement the exception handler and use it here.
+            return null;
+        }
+    }
+
+    /**
+     * Returns the parent of the object given as argument.
+     * 
+     * @param arg0
+     *            the TreeItem whom we want the parent.
+     * @return the parent as an Object.
+     * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+     */
+    public Object getParent(Object arg0) {
+        if (arg0 instanceof ISwtElement) {
+            return ((ISwtElement) arg0).getParent();
+        } else {
+            // TODO Implement the exception handler and use it here.
+            return null;
+        }
+    }
+
+    /**
+     * Indicates wether or not an element has children in the tree.
+     * 
+     * @param object
+     *            the element we wish to have an answer on.
+     * @return true if the element has children.
+     * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+     */
+    public boolean hasChildren(Object object) {
+        if (object instanceof ISwtElement) {
+            return ((ISwtElement) object).hasChildren();
+        } else {
+            // TODO Implement the exception handler and use it here.
+            return false;
+        }
+    }
+
+    /**
+     * Called whenever a TreeViewer.setinput() call is made.
+     * 
+     * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+     */
+    public Object[] getElements(Object object) {
+        if (object instanceof ISwtElement) {
+            return ((ISwtElement) object).getChildren();
+        }
+        // TODO Implement the exception handler and use it here.
+        return null;
+    }
+
+    /**
+     * Notifies this content provider that the given viewer's input has been
+     * switched to a different element. For now this is not used.
+     * 
+     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
+     *      java.lang.Object, java.lang.Object)
      */
     public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-        	System.out.println("Call to the inputChanged captured "+newInput);
+        System.out.println("New project tree input: " + newInput);
     }
 
 }
