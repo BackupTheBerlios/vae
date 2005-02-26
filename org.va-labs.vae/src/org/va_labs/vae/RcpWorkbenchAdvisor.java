ba416a33b5d6c093129832dfb581d1f54e2237a5
mojo_jojo <mojo_jojo>
2005-02-26 00:29:17 +0000

    (preWindowOpen): Changed the function to use the new architecture. Took out the coolbar as it is not used for now.

--- org.va-labs.vae/src/org/va_labs/vae/RcpWorkbenchAdvisor.java
+++ org.va-labs.vae/src/org/va_labs/vae/RcpWorkbenchAdvisor.java
@@ -1,7 +1,7 @@
 /*
  * Created on Jul 24, 2004
  *
- * $Id: RcpWorkbenchAdvisor.java,v 1.4 2004/09/05 20:25:09 mojo_jojo Exp $
+ * $Id: RcpWorkbenchAdvisor.java,v 1.5 2005/02/26 00:29:17 mojo_jojo Exp $
  */
 package org.va_labs.vae;
 
@@ -21,25 +21,29 @@ import org.va_labs.vae.gui.actions.OpenAction;
 import org.va_labs.vae.gui.actions.SaveAsAction;
 
 /**
- * @author mojo_jojo
+ * @author mojo_jojo 
+ * 
  * Customize the workbench for the Visual Ant Editor.
  */
-public class RcpWorkbenchAdvisor extends WorkbenchAdvisor 
-{
-    
+public class RcpWorkbenchAdvisor extends WorkbenchAdvisor {
+
     /**
      * Reference to the Visual Editor User Interface.
      */
     private Vui vui;
-    
+
     /**
      * Handles the creation of the menu of the RcpPerspective.
-     * @param window the workbench window.
-     * @param configurer the action bar configurer instance.
-     * @param flags bit mask indicating what needs to be filled
-     * or just described. (menu bar, cool bar, status line).
+     * 
+     * @param window
+     *            the workbench window.
+     * @param configurer
+     *            the action bar configurer instance.
+     * @param flags
+     *            bit mask indicating what needs to be filled or just described.
+     *            (menu bar, cool bar, status line).
      */
-    public void fillActionBars(IWorkbenchWindow window, 
+    public void fillActionBars(IWorkbenchWindow window,
             IActionBarConfigurer configurer, int flags) {
         IMenuManager menubar = configurer.getMenuManager();
         fillFileMenu(menubar, window);
@@ -49,54 +53,58 @@ public class RcpWorkbenchAdvisor extends WorkbenchAdvisor
 
     private void fillEditMenu(IMenuManager menubar, IWorkbenchWindow window) {
         MenuManager editMenu = new MenuManager("&Edit");
-        
+
         menubar.add(editMenu);
     }
+
     /**
      * Adds the File Menu to the given menubar.
-     * @param menubar menubar to which the File menu has to added.
+     * 
+     * @param menubar
+     *            menubar to which the File menu has to added.
      */
     private void fillFileMenu(IMenuManager menubar, IWorkbenchWindow window) {
         MenuManager fileMenu = new MenuManager("&File");
-		
-		fileMenu.add(new NewAction(vui));
-		fileMenu.add(new OpenAction(vui, window));
-		fileMenu.add(new Separator());
-		
-//		filemenu.add(new SaveAction(vui));
-		fileMenu.add(new SaveAsAction(vui, window));
-		fileMenu.add(new Separator());
-		
-		fileMenu.add(new ExitAction(vui));
-		
-		menubar.add(fileMenu);
+
+        fileMenu.add(new NewAction(vui));
+        fileMenu.add(new OpenAction(vui, window));
+        fileMenu.add(new Separator());
+
+        // Not ready yet.
+        //		filemenu.add(new SaveAction(vui));
+        fileMenu.add(new SaveAsAction(vui, window));
+        fileMenu.add(new Separator());
+
+        fileMenu.add(new ExitAction(vui));
+
+        menubar.add(fileMenu);
     }
-    
+
     private void fillHelpMenu(IMenuManager menubar, IWorkbenchWindow window) {
         MenuManager helpMenu = new MenuManager("&Help");
-        
+
         menubar.add(helpMenu);
     }
 
     /**
-     * Used to get the perspective id that we will be using for the visual
-     * ant editor.
+     * Used to get the perspective id that we will be using for the visual ant
+     * editor.
+     * 
      * @see org.eclipse.ui.application.WorkbenchAdvisor#getInitialWindowPerspectiveId()
      */
-    public String getInitialWindowPerspectiveId()
-    {
+    public String getInitialWindowPerspectiveId() {
         return "org.va_labs.vae.gui.RcpPerspective";
     }
-    
+
     public void preWindowOpen(IWorkbenchWindowConfigurer configurer) {
         super.preWindowOpen(configurer);
         configurer.setInitialSize(new Point(800, 600));
         configurer.setTitle(Messages.getString("VAE"));
-        
+        configurer.setShowCoolBar(false);
+
         // Initializes the Vae core, and the user interface.
-        Vae.getVae();
-        vui = Vui.getVui();
+        Vae vae = Vae.getInstance();
+        vui = vae.getVui();
         vui.registerConfigurer(configurer);
     }
-
 }
