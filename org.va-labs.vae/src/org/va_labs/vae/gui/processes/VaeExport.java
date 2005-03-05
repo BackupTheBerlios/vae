b514d2a40446b6d4f6dab9054f7759868f9dc7a5
mojo_jojo <mojo_jojo>
2005-03-05 15:47:40 +0000

    (getInstance): New method to get the VaeExport singleton. (VaeExport): Nothing to be done in this for now.

--- org.va-labs.vae/src/org/va_labs/vae/gui/processes/VaeExport.java
+++ org.va-labs.vae/src/org/va_labs/vae/gui/processes/VaeExport.java
@@ -1,15 +1,40 @@
 /*
  * Created on Sep 4, 2004
  *
- * $Id: VaeExport.java,v 1.1 2004/09/05 00:18:45 mojo_jojo Exp $
+ * $Id: VaeExport.java,v 1.2 2005/03/05 15:47:40 mojo_jojo Exp $
  */
 package org.va_labs.vae.gui.processes;
 
 /**
  * @author mojo_jojo
  *
- * TODO Comment the  class.
+ * Exports the build file in various format.
  */
 public class VaeExport {
-
+    
+    /**
+     * Singleton instance of VaeExport class.
+     */
+    private static VaeExport vaeExport = null;
+    
+    /**
+     * Gives the lone instance of VaeExport class.
+     * 
+     * Creates the class if needed.
+     * @return the VaeExport singleton.
+     */
+    public static VaeExport getInstance() {
+        if (vaeExport == null) {
+            vaeExport = new VaeExport();
+        }
+        return vaeExport;
+    }
+    
+    /**
+     * Nothing to be done for now.
+     *
+     */
+    public VaeExport() {
+        
+    }
 }
