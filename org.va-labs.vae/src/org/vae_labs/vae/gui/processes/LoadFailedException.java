1329cd09095a410b02776aa466e3f53e4bfa9687
mojo_jojo <mojo_jojo>
2006-05-06 19:19:39 +0000

    * LoadFailedException.java : Added a serial version UID for serialization.

--- org.va-labs.vae/src/org/vae_labs/vae/gui/processes/LoadFailedException.java
+++ org.va-labs.vae/src/org/vae_labs/vae/gui/processes/LoadFailedException.java
@@ -1,7 +1,7 @@
 /*
  * Created on Sep 4, 2004
  *
- * $Id: LoadFailedException.java,v 1.1 2005/04/05 02:45:23 mojo_jojo Exp $
+ * $Id: LoadFailedException.java,v 1.2 2006/05/06 19:19:39 mojo_jojo Exp $
  */
 package org.vae_labs.vae.gui.processes;
 
@@ -12,16 +12,20 @@ import org.vae_labs.vae.core.Vae;
 
 /**
  * @author mojo_jojo
- *
- * Exception Thrown in case the Loading of a build file failed.
- * It sets automatically the module to Build Loader.
- * The message and the reason are directly from the exception. 
+ * 
+ * Exception Thrown in case the Loading of a build file failed. It sets
+ * automatically the module to Build Loader. The message and the reason are
+ * directly from the exception.
  */
 public class LoadFailedException extends VaeException {
-    
-	public LoadFailedException(InvocationTargetException e)
-	{
-		super("Ant Loader", "Loading of ant build file failed.",
-				e.getCause().getMessage(), Vae.VAE__OK);
+
+	/**
+	 * Used for serialization purposes.
+	 */
+	private static final long serialVersionUID = -1808488384261621759L;
+
+	public LoadFailedException(InvocationTargetException e) {
+		super("Ant Loader", "Loading of ant build file failed.", e.getCause()
+				.getMessage(), Vae.VAE__OK);
 	}
 }
