91ebd2262b4e905620f74bf9b5532a24291b3b48
mojo_jojo <mojo_jojo>
2006-05-06 19:17:49 +0000

    * IllegalAttributeException.java : Added a serial version UID for serialization.
    * VaeParsingException.java : Added a serial version UID for serialization.

--- org.va-labs.vae/src/org/vae_labs/vae/parser/IllegalAttributeException.java
+++ org.va-labs.vae/src/org/vae_labs/vae/parser/IllegalAttributeException.java
@@ -1,7 +1,7 @@
 /*
  * Created on Aug 13, 2004
  *
- * $Id: IllegalAttributeException.java,v 1.1 2005/04/05 02:45:24 mojo_jojo Exp $
+ * $Id: IllegalAttributeException.java,v 1.2 2006/05/06 19:17:49 mojo_jojo Exp $
  */
 package org.vae_labs.vae.parser;
 
@@ -9,30 +9,38 @@ import org.vae_labs.vae.VaeException;
 import org.vae_labs.vae.core.Vae;
 
 /**
- * @author mojo_jojo This exception is intanciated if the user tried to add an
- *         Attribute that is not legal for the component.
+ * This exception is intanciated if the user tried to add an Attribute that is
+ * not legal for the component.
+ * 
+ * @author mojo_jojo
+ * 
  */
 public class IllegalAttributeException extends VaeException {
 
-    /**
-     * Creates an IllegalVaeAttribute exception indicating wich attribute caused
-     * problem, what is the element it caused problem for, and a list of legal
-     * Attributes for the element.
-     * 
-     * @param attributeName
-     *            the name of attribute that has been rejected.
-     * @param elementName
-     *            the name of the element the attribute is not legal for.
-     * @param legalAttributes
-     *            list of legal attributes for this element.
-     */
-    public IllegalAttributeException(String attributeName, String elementName,
-            String[] legalAttributes) {
-        super("Vae User Interface", "Wrong Attribute", "Legal attributes for "
-                + elementName + " : \n    ", Vae.VAE__OK);
-        reason.append(legalAttributes[0]);
-        for (int i=1; i < legalAttributes.length; i++) {
-            reason.append(", " + legalAttributes[i]);
-        }
-    }
+	/**
+	 * Used for serialization purposes.
+	 */
+	private static final long serialVersionUID = 6474279564735710036L;
+
+	/**
+	 * Creates an IllegalVaeAttribute exception indicating wich attribute caused
+	 * problem, what is the element it caused problem for, and a list of legal
+	 * Attributes for the element.
+	 * 
+	 * @param attributeName
+	 *            the name of attribute that has been rejected.
+	 * @param elementName
+	 *            the name of the element the attribute is not legal for.
+	 * @param legalAttributes
+	 *            list of legal attributes for this element.
+	 */
+	public IllegalAttributeException(String attributeName, String elementName,
+			String[] legalAttributes) {
+		super("Vae User Interface", "Wrong Attribute", "Legal attributes for "
+				+ elementName + " : \n    ", Vae.VAE__OK);
+		reason.append(legalAttributes[0]);
+		for (int i = 1; i < legalAttributes.length; i++) {
+			reason.append(", " + legalAttributes[i]);
+		}
+	}
 }
--- org.va-labs.vae/src/org/vae_labs/vae/parser/VaeParsingException.java
+++ org.va-labs.vae/src/org/vae_labs/vae/parser/VaeParsingException.java
@@ -1,7 +1,7 @@
 /*
  * Created on Sep 4, 2004
  *
- * $Id: VaeParsingException.java,v 1.1 2005/04/05 02:45:24 mojo_jojo Exp $
+ * $Id: VaeParsingException.java,v 1.2 2006/05/06 19:17:49 mojo_jojo Exp $
  */
 package org.vae_labs.vae.parser;
 
@@ -9,24 +9,32 @@ import org.vae_labs.vae.VaeException;
 
 /**
  * @author mojo_jojo
- *
- * Exception thrown when the Vae xml parser faces an error while parsing
- * a build file.
- * For now, nothing more than a VaeException.
+ * 
+ * Exception thrown when the Vae xml parser faces an error while parsing a build
+ * file. For now, nothing more than a VaeException.
  */
 public class VaeParsingException extends VaeException {
 
-    /**
-     * Creates a Vae Parsing Exception
-     * @param vaeModule vae module issuing the exception.
-     * @param errorMessage error message to be prompted to the user.
-     * @param errorReason reason for which the exception has been raised.
-     * @param moduleStatus status of the issuing module after the exception has
-     * been raise.
-     */
-    public VaeParsingException(String vaeModule, String errorMessage, 
-			String errorReason, int moduleStatus)
-	{
+	/**
+	 * Used for serialization purposes.
+	 */
+	private static final long serialVersionUID = 580183640659989402L;
+
+	/**
+	 * Creates a Vae Parsing Exception
+	 * 
+	 * @param vaeModule
+	 *            vae module issuing the exception.
+	 * @param errorMessage
+	 *            error message to be prompted to the user.
+	 * @param errorReason
+	 *            reason for which the exception has been raised.
+	 * @param moduleStatus
+	 *            status of the issuing module after the exception has been
+	 *            raise.
+	 */
+	public VaeParsingException(String vaeModule, String errorMessage,
+			String errorReason, int moduleStatus) {
 		super(vaeModule, errorMessage, errorReason, moduleStatus);
 	}
 }
